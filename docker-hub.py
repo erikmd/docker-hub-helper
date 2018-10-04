c1f8aa4c524dc7a101fd2f403565a5b274457b92
Erik Martin-Dorel <erik@martin-dorel.org>
2018-10-04 13:53:40 +0200

    Replace "not(local>remote)" with "not(local>remote) && local!=remote"
    Rely on "git rev-parse --verify COMMIT_REF"

--- docker-hub.py
+++ docker-hub.py
@@ -47,6 +47,10 @@ def setminus(s1, s2):
     return list(filter(lambda e: e not in s2, s1))
 
 
+def chomp(byt):
+    return byt.decode('UTF-8').rstrip()
+
+
 def fetch(repo):
     check_call(["git", "fetch", "origin"],
                cwd=os.path.expanduser(repo))
@@ -60,7 +64,7 @@ def all_remote_branches(repo):
                         "--format", "%(refname:strip=3)",
                         "refs/remotes/origin/"],
                        cwd=os.path.expanduser(repo))
-    return byt.decode('UTF-8').rstrip().split('\n')
+    return chomp(byt).split('\n')
 
 
 def all_local_branches(repo):
@@ -68,7 +72,7 @@ def all_local_branches(repo):
                         "--format", "%(refname:strip=2)",
                         "refs/heads/"],
                        cwd=os.path.expanduser(repo))
-    return byt.decode('UTF-8').rstrip().split('\n')
+    return chomp(byt).split('\n')
 
 
 def local_newer(repo, b, remote_b=''):
@@ -93,6 +97,16 @@ def remote_newer(repo, b, remote_b=''):
     # return 1 if remote_b is newer than remote_b
     # 128 if remote_b doesn't exist
     return ret == 1
+
+
+def local_remote_neq(repo, b, remote_b=''):
+    if not remote_b:
+        remote_b = b + '@{u}'
+    wd = os.path.expanduser(repo)
+    sha_b, sha_remote_b = [chomp(check_output(["git", "rev-parse",
+                                               "--verify", x],
+                                              cwd=wd)) for x in [b, remote_b]]
+    return sha_b != sha_remote_b
 
 
 def branches(repo):
@@ -165,8 +179,8 @@ def rebase(repo, all, branch):
     wd = os.path.expanduser(repo)
     for b in brs:
         remote_b = 'origin/' + b
-        needs_pull = not local_newer(repo, b, remote_b)
-        # (!) needs_pull could be further constrained with a SHA1 test
+        needs_pull = (not local_newer(repo, b, remote_b)) and \
+            local_remote_neq(repo, b, remote_b)
         newer = remote_b if needs_pull else b
         print("\n* Rebasing %s on master..." % newer,
               file=sys.stderr, flush=True)
